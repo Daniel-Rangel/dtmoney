3e59b5a151c12a8917f9cdf2add50d7da98ae31e
Daniel Rangel <danf.arangel@gmail.com>
2022-02-25 22:56:34 -0300

    primeiro comit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+font-size : 24px;
+  color: #8257e6;
+`
+
+export function App() {
+  return (
+    <Title className="App">
+      <h1>ola mundo</h1>
+    </Title>
+  );
+}
+
+
